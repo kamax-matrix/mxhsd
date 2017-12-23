15b1135e363eb8c450e0da7a44b2b10adedf207b
Maxime Dor <max@kamax.io>
2017-12-23 05:43:11 +0100

    Skeleton to act as a client in federation (part 1)

---
+++ src/main/java/io/kamax/mxhsd/api/federation/FederationDomainResolver.java
@@ -0,0 +1,78 @@
+/*
+ * mxhsd - Corporate Matrix Homeserver
+ * Copyright (C) 2017 Maxime Dor
+ *
+ * https://www.kamax.io/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.kamax.mxhsd.api.federation;
+
+import com.google.common.net.InetAddresses;
+
+public class FederationDomainResolver {
+
+    public static class Builder {
+
+        private FederationDomainResolver r = new FederationDomainResolver();
+
+        public Builder withDefaultPort(int port) {
+            r.port = port;
+            return this;
+        }
+
+        public Builder withDnsPrefix(String prefix) {
+            r.prefix = prefix;
+            return this;
+        }
+
+        public FederationDomainResolver get() {
+            try {
+                return r;
+            } finally {
+                r = null;
+            }
+        }
+    }
+
+    public static Builder build() {
+        return new Builder();
+    }
+
+    private int port = 8448;
+    private String prefix = "_matrix._tcp.";
+
+    public String resolve(String domain) {
+        // This is a literal IP address without any port
+        // We add the default port and return the value
+        if (InetAddresses.isInetAddress(domain)) {
+            return InetAddresses.toUriString(InetAddresses.forString(domain)) + ":" + port;
+        }
+
+        // This is an IP address with a port in it
+        // We return verbatim
+        if (InetAddresses.isUriInetAddress(domain)) {
+            return domain;
+        }
+
+
+        if (domain.contains(":")) {
+            return domain.toLowerCase();
+        }
+
+        return domain;
+    }
+
+}
---
+++ src/test/java/io/kamax/mxhsd_test/api/federation/FederationDomainResolverTest.java
@@ -0,0 +1,76 @@
+/*
+ * mxhsd - Corporate Matrix Homeserver
+ * Copyright (C) 2017 Maxime Dor
+ *
+ * https://www.kamax.io/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.kamax.mxhsd_test.api.federation;
+
+import io.kamax.mxhsd.api.federation.FederationDomainResolver;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+public class FederationDomainResolverTest {
+
+    private FederationDomainResolver getResolver() {
+        FederationDomainResolver o = FederationDomainResolver.build().get();
+        assertNotNull(o);
+        return o;
+    }
+
+    @Test
+    public void validIPv4Literal() {
+        String address = "127.0.0.1";
+        String expected = "127.0.0.1:8448";
+
+        FederationDomainResolver o = getResolver();
+        String resolved = o.resolve(address);
+
+        assertTrue(resolved, StringUtils.equals(expected, resolved));
+    }
+
+    @Test
+    public void validIPv6Literal() {
+        String address = "::1";
+        String expected = "[::1]:8448";
+
+        FederationDomainResolver o = getResolver();
+        String resolved = o.resolve(address);
+
+        assertTrue(resolved, StringUtils.equals(expected, resolved));
+    }
+
+    @Test
+    public void validHostnameWithValidPort() {
+        String address = "localhost:1";
+        String expected = "localhost:1";
+
+        FederationDomainResolver o = getResolver();
+        String resolved = o.resolve(address);
+
+        assertTrue(resolved, StringUtils.equals(expected, resolved));
+    }
+
+    @Test
+    public void validHostnameLiteral() {
+        // FIXME do test
+    }
+
+}
